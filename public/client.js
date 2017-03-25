fffe5ae1077d323d96c361e9d4d115af6baed833
System <none>
2017-03-25 04:47:13 +0000

    👄🌶 Some frontend stuff

--- public/client.js
+++ public/client.js
@@ -6,36 +6,4 @@
 
 $(function() {
   console.log('hello world :o');
-  
-  var currentLink = window.location.href
-  console.log(currentLink);
-  var link = getParameterByName("link", currentLink)
-  console.log("Passed link: " + link);
-  if (link != null) {
-    $("#bytes").val(link);
-  }
-  $( "#follow" ).click(function() {
-      goToLink();
-  });
-  $("#bytes").keyup(function(event){
-    if(event.keyCode == 13){
-      goToLink();
-    }
-  });
-  function goToLink() {
-    var link = $("#bytes").val();
-    var win = window.open(link, "_blank");
-  }
-  function getParameterByName(name, url) {
-      if (!url) {
-        url = window.location.href;
-      }
-      name = name.replace(/[\[\]]/g, "\\$&");
-      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-          results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
-
 });
