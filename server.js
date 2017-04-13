7e46fa9900e0f0ad68600b2ab629c386463b4d14
Mythen <mythen@outlook.de>
2017-04-13 11:52:36 +0200

    removed the mess my ide was doing

--- server.js
+++ server.js
@@ -3,19 +3,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-<<<<<<< dc9160c2d6fedc75da903679eb9ff4b793206581
-var gitlab = require('./gitlab')
-var unity = require('./unity')
-var travis = require('./travis')
-var circleci = require('./circleci')
-var appveyor = require('./appveyor')
-=======
 var gitlab = require('./providers/gitlab');
 var travis = require('./providers/travis');
 var circleci = require('./providers/circleci');
 var appveyor = require('./providers/appveyor');
->>>>>>> put providers in its owner folder for better visibility.
-
+var unity = require('./providers/unity');
 var app = express();
 
 app.use(bodyParser.json());
@@ -34,41 +26,6 @@ app.post("/api/webhooks/:hookPart1/:hookPart2/:from", function (req, res) {
     var from = req.params.from;
     var discordHookUrl = "https://discordapp.com/api/webhooks/" + hookPart1 + "/" + hookPart2;
 
-<<<<<<< dc9160c2d6fedc75da903679eb9ff4b793206581
-  // https://discordapp.com/developers/docs/resources/webhook#execute-webhook
-  var discordPayload = new Object()
-  switch(from) {
-    case "gitlab":
-      gitlab.parse(req, discordPayload)
-      break;
-    case "travis":
-      travis.parse(req, discordPayload)
-      break;
-    case "circleci":
-      circleci.parse(req, discordPayload)
-      break;
-    case "appveyor":
-      appveyor.parse(req, discordPayload)
-      break;
-    case "unity":
-      unity.parse(req, discordPayload)
-      break;
-    default:
-      console.log("Unknown from: " + from)
-      //todo return some error
-  }
-
-   var jsonString= JSON.stringify(discordPayload)
-  request.post({
-    headers: {'content-type' : 'application/json'},
-    url:     discordHookUrl,
-    body:    jsonString
-  }, function(error, response, body){
-    if (error) {
-      res.sendStatus(400)
-    } else {
-      res.sendStatus(200)
-=======
     // https://discordapp.com/developers/docs/resources/webhook#execute-webhook
     var discordPayload = {
         username: "skyhook" // feel free to remove this line. It overwrites the set username of the webhook in discord. So the username that posts will be skyhook regardless what is set in discord.
@@ -86,10 +43,12 @@ app.post("/api/webhooks/:hookPart1/:hookPart2/:from", function (req, res) {
         case "appveyor":
             appveyor.parse(req, discordPayload);
             break;
+        case "unity":
+            unity.parse(req, discordPayload);
+            break;
         default:
             console.log("Unknown from: " + from)
         //todo return some error
->>>>>>> put providers in its owner folder for better visibility.
     }
 
     var jsonString = JSON.stringify(discordPayload);
@@ -99,12 +58,11 @@ app.post("/api/webhooks/:hookPart1/:hookPart2/:from", function (req, res) {
         body: jsonString
     }, function (error, response, body) {
         if (error) {
-            res.sendStatus(400);
+            res.sendStatus(400)
         } else {
-            res.sendStatus(200);
+            res.sendStatus(200)
         }
     });
-
 });
 
 // listen for requests :)
