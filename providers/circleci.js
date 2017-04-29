6709680aca7863312ce61cd29a5998e3f5bf874c
Mythen <mythen@outlook.de>
2017-04-29 02:33:25 +0200

    Changed CircleCi layout

--- providers/circleci.js
+++ providers/circleci.js
@@ -4,9 +4,14 @@
 module.exports = {
     parse: function (req, discordPayload) {
         var body = req.body;
-        var id = body.payload.build_num;
-        var buildUrl = body.payload.build_url;
-        var outcome = body.payload.outcome;
-        discordPayload.content = "Build " + id + " " + outcome + "\n" + buildUrl
+        discordPayload.setEmbedColor(0x000000);
+        discordPayload.addEmbed({
+            title: "Build #" + body.payload.build_num,
+            url: body.payload.build_url,
+            author: {
+                name: body.payload.reponame
+            },
+            description: "**Outcome**: " + build.payload.outcome
+        });
     }
 };
