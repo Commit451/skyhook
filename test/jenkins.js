85ce1efa8497201f36b2834a6cdfcca01a30af3c
Mythen <mythen@outlook.de>
2017-06-15 10:08:01 +0200

    Jenkins support

---
+++ test/jenkins.js
@@ -0,0 +1,43 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const json = {
+    name: 'Test',
+    url: 'job/Test/',
+    build: {
+        full_url: 'http://jenkins-url/job/Test/25/',
+        number: 25,
+        queue_id: 25,
+        phase: 'FINALIZED',
+        status: 'SUCCESS',
+        url: 'job/Test/25/',
+        scm: {},
+        log: '',
+        artifacts: {}
+    }
+};
+
+/*
+ * Test the /POST route
+ */
+describe('/POST jenkins', () => {
+    it('deploy', (done) => {
+        chai.request(server)
+            .post('/api/webhooks/test/test/jenkins')
+            .set("test", "true")
+            .send(json)
+            .end((err, res) => {
+                res.should.have.status(200);
+                console.log(res.body);
+                res.body.should.be.a('object');
+                res.body.should.have.property('embeds');
+                done();
+            });
+    });
+});
