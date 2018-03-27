c567689fa2c50e02cdbd58672df6fd4ab2158872
John <jawnnypoo@gmail.com>
2018-03-26 23:49:16 -0500

    Space in description for Pingdom

--- providers/pingdom.js
+++ providers/pingdom.js
@@ -9,11 +9,11 @@ class Pingdom extends BaseProvider {
     }
 
     async parseData() {
-        if (this.body.current_state != this.body.previous_state) {
+        if (this.body.current_state !== this.body.previous_state) {
             this.payload.setEmbedColor((this.body.current_state === "UP") ? 0x4caf50 : 0xd32f2f);
             this.payload.addEmbed({
                 title: "State changed",
-                description: "State change from" + this.body.previous_state + " to " + this.body.current_state
+                description: "State change from " + this.body.previous_state + " to " + this.body.current_state
             });
         }
     }
