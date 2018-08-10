d69b7b6878747fcc77c70045587b0d0217d80f4f
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2018-08-10 07:42:37 +0700

    Revised

--- src/provider/Jira.ts
+++ src/provider/Jira.ts
@@ -19,20 +19,22 @@ class Jira extends BaseProvider {
     public async parseData() {
         this.setEmbedColor(0x1e45a8)
 
-        var issue = this.body.issue;
+        const issue = this.body.issue
         if (issue.fields.assignee === null) {
-            issue.fields.assignee = {displayName: "nobody"};
+            issue.fields.assignee = {displayName: 'nobody'}
         }
-    
-        var user = this.body.user;
-        var action = this.body.issue_event_type_name.split('_')[1];
-        var matches = issue.self.match(/^(https?:\/\/[^\/?#]+)(?:[\/?#]|$)/i);
-        var domain = matches && matches[1];
-    
+
+        // extract variable from Jira
+        const user = this.body.user
+        const action = this.body.issue_event_type_name.split('_')[1]
+        const matches = issue.self.match(/^(https?:\/\/[^\/?#]+)(?:[\/?#]|$)/i)
+        const domain = matches && matches[1]
+
+        // embed builder
         const embed = new Embed()
         embed.title = `${issue.key} - ${issue.fields.summary}`
         embed.description = `${user.displayName} ${action} the issue ${embed.title} (${issue.fields.assignee.displayName})`
-        embed.url = `${domain}/browse/${issue.key}`;
+        embed.url = `${domain}/browse/${issue.key}`
         this.addEmbed(embed)
     }
 }
