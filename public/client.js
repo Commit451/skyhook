0334afe46db829c85a2db11cf9e107b78b5c7ebe
Daniel Scalzi <d_scalzi@yahoo.com>
2018-08-22 11:18:41 -0400

    Dynamically resolve hostname in client.js (#100).

--- public/client.js
+++ public/client.js
@@ -16,28 +16,28 @@ window.mdc.autoInit();
         sb.show(data);
     };
 
-    $("#button-generate").click(function () {
+    $('#button-generate').click(function () {
         let discordHookUrl = $('#url').val();
         let error = false;
-        if (discordHookUrl && discordHookUrl.includes("discordapp.com")) {
+        if (discordHookUrl && discordHookUrl.includes('discordapp.com')) {
 
-            const endSpacialPart = discordHookUrl.indexOf("discordapp.com");
-            const startSpacialPart = (discordHookUrl.indexOf(":") + 3);// + :// 3 chars:)
+            const endSpacialPart = discordHookUrl.indexOf('discordapp.com');
+            const startSpacialPart = (discordHookUrl.indexOf(':') + 3);// + :// 3 chars:)
 
-            discordHookUrl = discordHookUrl.replace(discordHookUrl.substring(startSpacialPart, endSpacialPart), "");
-            discordHookUrl = discordHookUrl.replace("discordapp.com", "skyhook.glitch.me");
+            discordHookUrl = discordHookUrl.replace(discordHookUrl.substring(startSpacialPart, endSpacialPart), '');
+            discordHookUrl = discordHookUrl.replace('https://discordapp.com', window.location.origin);
 
             const provider = $('input[name=ex2]:checked').val();
-            discordHookUrl = discordHookUrl + "/" + provider;
+            discordHookUrl = discordHookUrl + '/' + provider;
         } else {
             error = true;
         }
 
         if (!error) {
             window.copyToClipboard(discordHookUrl);
-            show(snackbar, "URL Generated. Copied to Clipboard");
+            show(snackbar, 'URL Generated. Copied to Clipboard');
         } else {
-            show(snackbar, "Unable to create URL. Make sure your Discord URL is valid");
+            show(snackbar, 'Unable to create URL. Make sure your Discord URL is valid');
         }
     });
 
