db11deabddffb479cdcfffe593779fdf9a690d73
Mandeep Gill <mandeep@nstack.com>
2019-01-21 13:50:26 +0000

    feat/jira: Add support for Jira comments

--- src/provider/Jira.ts
+++ src/provider/Jira.ts
@@ -22,32 +22,33 @@ class Jira extends BaseProvider {
     public async parseData() {
         this.setEmbedColor(0x1e45a8)
 
-        const issue = this.body.issue
-
         // We don't support anything else at this time.
-        if (issue == null) {
+        if (!this.body.webhookEvent.startsWith('jira:issue_')) {
             return
         }
+
+        // extract variable from Jira
+        const issue = this.body.issue
+        const comment = this.body.comment
+        const user = this.body.user
+        const action = this.body.webhookEvent.split('_')[1]
+        const matches = issue.self.match(/^(https?:\/\/[^\/?#]+)(?:[\/?#]|$)/i)
+        const domain = matches && matches[1]
 
         if (issue.fields.assignee == null) {
             issue.fields.assignee = {displayName: 'nobody'}
         }
 
-        // extract variable from Jira
-        let user = this.body.user
-        let action = this.body.webhookEvent.split('_')[1]
-        const matches = issue.self.match(/^(https?:\/\/[^\/?#]+)(?:[\/?#]|$)/i)
-        const domain = matches && matches[1]
-
-        if (user == null && this.body.comment != null) {
-            user = this.body.comment.updateAuthor
-            action = 'commented on'
-        }
-
         // embed builder
         const embed = new Embed()
         embed.title = `${issue.key} - ${issue.fields.summary}`
-        embed.description = `${user.displayName} ${action} the issue ${embed.title} (${issue.fields.assignee.displayName})`
+
+        if (comment != null) {
+            embed.description = `${comment.updateAuthor.displayName} commented: ${comment.body}`
+        } else {
+            embed.description = `${user.displayName} ${action} the issue ${embed.title} (${issue.fields.assignee.displayName})`
+        }
+
         embed.url = `${domain}/browse/${issue.key}`
         this.addEmbed(embed)
     }
--- test/jiraTests.ts
+++ test/jiraTests.ts
@@ -4,8 +4,16 @@ import { Tester } from './Tester'
 
 describe('/POST jira', () => {
     it('issue_updated', async () => {
-        const res = await Tester.test(new Jira(), 'jira.json', null)
+        const res = await Tester.test(new Jira(), 'jira-issue.json', null)
         expect(res).to.not.be.an('error')
         expect(res).to.not.be.a('DiscordPayload')
     })
+
+    it('comment_added', async () => {
+        const res = await Tester.test(new Jira(), 'jira-comment.json', null)
+        expect(res).to.not.be.an('error')
+        expect(res).to.not.be.a('DiscordPayload')
+    })
+
+
 })
