0a852e3046dc3cf0d154354d7071b6b65f4583c3
Daniel Scalzi <d_scalzi@yahoo.com>
2019-03-27 01:36:54 -0400

    Improve Travis Format and Handling.

--- src/provider/Travis.ts
+++ src/provider/Travis.ts
@@ -7,16 +7,12 @@ import { BaseProvider } from '../provider/BaseProvider'
  */
 class Travis extends BaseProvider {
 
+    // States https://github.com/travis-ci/travis-api/blob/master/lib/travis/model/build/states.rb#L25
     private static status_colors = {
-        'still broken': 0xff0000,
-        'still failing': 0xb04632,
-        broken: 0xff0000,
-        pending: 0x04d9e3,
-        failed: 0x519839,
-        errored: 0x89609e,
-        fixed: 0xcd5a91,
-        passed: 0x4bbf6b,
-        travis: 0xccddff
+        passed: 0x39aa56,
+        failed: 0xdb4545,
+        errored: 0xdb4545,
+        canceled: 0x9d9d9d
     }
 
     public getName() {
@@ -24,47 +20,33 @@ class Travis extends BaseProvider {
     }
 
     public async parseData() {
-        this.setEmbedColor(0xC7B398)
+        this.setEmbedColor(0x39aa56)
         const embed = new Embed()
         let target_body = this.body
-        if (this.body.payload !== null && typeof this.body.payload === "string") {
-            // for some reason, Travis-CI has switched up on us and
-            // is sending the payload nested, rather than top-level
-            // set a different body and parse it appropriately
-            this.logger.debug('Travis-CI is not following its own documentation and so we are parsing the body.payload string.');
+        if (this.body.payload != null && typeof this.body.payload === 'string') {
+            // Travis now sends data inside of a string payload property.
             try {
                 target_body = JSON.parse(this.body.payload)
             }
             catch (error) {
-                this.logger.info('falling back to body, failed to parse body.payload string: ' + error.stack)
-                // do not care about errors here;
-                // just go back to using usual body and pray it works
+                this.logger.info('Malformed payload JSON from travis.')
+                this.logger.error(error)
                 target_body = this.body
             }
         }
-        embed.title = 'Build #' + target_body.number
+
+        embed.title = `[${target_body.repository.name}:${target_body.branch}] Build #${target_body.number}: ${target_body.status_message}`
         embed.url = target_body.build_url
-        embed.description = '**Branch**: ' + target_body.branch + '\n**Status**: ' + target_body.status_message
-        const author = new EmbedAuthor()
-        author.name = this.headers['Travis-Repo-Slug'] || this.headers['travis-repo-slug'] || (target_body.repository.owner_name + '/' + target_body.repository.name)
-        embed.author = author
-        if (typeof target_body.status_message === 'string') {
-            const status: string = target_body.status_message.toLowerCase();
-            embed.color = Travis.status_colors['travis']
-            if (Travis.status_colors[status] !== null) {
-                embed.color = Travis.status_colors[status]
-            }
-            else {
-                Object.getOwnPropertyNames(Travis.status_colors).every(
-                    function (element: string, index: number, color_names: string[]) {
-                        if (status.indexOf(element) > -1) {
-                            embed.color = Travis.status_colors[element]
-                            return false
-                        }
-                    }
-                )
+        embed.description = `[\`${target_body.commit.substring(0, 7)}\`](${target_body.compare_url}) ${target_body.message}`
+
+        if(target_body.state != null){
+            if(Travis.status_colors[target_body.state] != null) {
+                this.setEmbedColor(Travis.status_colors[target_body.state])
+            } else {
+                this.logger.warn('Unknown Travis build state: ' + target_body.state)
             }
         }
+
         this.addEmbed(embed)
     }
 }
