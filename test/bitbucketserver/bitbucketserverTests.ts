7e227c0fd3600e64dc6c8b6031d9d8d7c731cdc2
cryptic-ai <11448029+cryptic-ai@users.noreply.github.com>
2020-04-12 18:42:07 -0400

    Added a new test to ensure that there are not over 18 fields attached

--- test/bitbucketserver/bitbucketserverTests.ts
+++ test/bitbucketserver/bitbucketserverTests.ts
@@ -13,3 +13,16 @@ describe('/POST bitbucketserver', () => {
         expect(res).to.not.be.a('DiscordPayload')
     })
 })
+
+describe('/POST bitbucketserver', () => {
+    it('repo:refs_changed 18 fields or less', async() => {
+        const headers = {
+            'x-event-key': 'repo:refs_changed'
+        }
+
+        const res = await Tester.test(new BitBucketServer(), './bitbucketserver/bitbucketserver.json', headers)
+
+
+        expect(res.embeds[0].fields.length).to.not.be.greaterThan(18)
+    })
+})
