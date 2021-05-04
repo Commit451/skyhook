3c6b985aea28e56458691b3c05082060ab8cd710
Felipe de Souza Paiva <spaiva.felipe@gmail.com>
2021-05-04 12:28:12 -0300

    Update Pingdom.js
    Add the HTTP Check Name on the Discord alert.
    
    From:
    State changed
    State change from UP to DOWN
    
    To:
    WEBSITE ACME - State changed
    State change from UP to DOWN

--- src/provider/Pingdom.ts
+++ src/provider/Pingdom.ts
@@ -13,7 +13,7 @@ class Pingdom extends BaseProvider {
     public async parseData(): Promise<void> {
         if (this.body.current_state !== this.body.previous_state) {
             const embed = new Embed()
-            embed.title = 'State changed'
+            embed.title = this.body.check_name + ' - State changed'
             embed.description = 'State change from ' + this.body.previous_state + ' to ' + this.body.current_state
             this.setEmbedColor((this.body.current_state === 'UP') ? 0x4caf50 : 0xd32f2f)
             this.addEmbed(embed)
