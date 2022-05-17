b851ac1de04c6de3d10ae67aca02c9f9b9a23e27
John Carlson <jawnnypoo@gmail.com>
2022-05-17 02:09:28 -0500

    API for server info

--- src/index.ts
+++ src/index.ts
@@ -92,18 +92,21 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
-app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.redirect('https://commit451.github.io/skyhook-web');
 })
 
-app.get('/providers', (req, res) => {
-    res.status(200).send(providerNames)
+app.get('/api/providers', (_req, res) => {
+    res.status(200).send(providerInfos)
 })
 
-app.get('/api/providers', (req, res) => {
-    res.status(200).send(providerInfos)
+const info = {
+    version: process.env.GAE_VERSION,
+    deployment: process.env.GAE_DEPLOYMENT_ID,
+}
+app.get('/api/info', (_req, res) => {
+    res.status(200).send(info)
 })
 
 app.get('/api/webhooks/:webhookID/:webhookSecret/:from', (req, res) => {
@@ -189,7 +192,7 @@ app.post('/api/webhooks/:webhookID/:webhookSecret/:from/test', async (req, res) 
 app.use(Sentry.Handlers.errorHandler())
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((req, res, next) => {
+app.use((_req, res, _next) => {
     res.status(404).send('Not Found')
 })
 
